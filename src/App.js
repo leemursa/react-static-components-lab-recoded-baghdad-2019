66e2a3397835a661d61f38216acaff017733463a
Mursaleen Taqi <leemursa@gmail.com>
2020-01-25 13:32:47 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from "react";
 import CatComponent from "./CatComponent.js";
 import EinsteinQuoteComponent from "./EinsteinQuoteComponent.js";
@@ -16,3 +17,23 @@ class App extends Component {
 }
 
 export default App;
+=======
+import React, { Component } from 'react';
+import CatComponent from './CatComponent.js'
+import FordQuoteComponent from './FordQuoteComponent.js'
+import MouseComponent from './MouseComponent.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CatComponent />
+        <FordQuoteComponent />
+        <MouseComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
+>>>>>>> df1fb0641fb72491a4a809d33a0a772d76ff9553
